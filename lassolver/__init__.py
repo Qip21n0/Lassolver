2f7a06d20fed0ef6f82e89080f90cd6d3a39ba97
Kenn <h.ken.40nuj7991@icloud.com>
2020-12-16 13:48:11 +0900

    add 0st version

--- lassolver/__init__.py
+++ lassolver/__init__.py
@@ -1,2 +1,11 @@
+import lassolver.matrices
 import lassolver.solver
 import lassolver.utils
+
+def version():
+    version = '0.0.0'
+    
+    return """
+    Lassolver ver {}.
+    """.format(version)
+
