0d04503fe3601cb8b2689ebdb9f934575116d22a
Kenn <h.ken.40nuj7991@icloud.com>
2020-12-23 01:19:57 +0900

    add estimate func

--- lassolver/dsolver/d_amp.py
+++ lassolver/dsolver/d_amp.py
@@ -6,13 +6,13 @@ class D_AMP:
         self.M, self.N = A.shape
         self.P = P
         self.a = self.M / self.N
-        self.b = int(self.M / self.P)
-        self.A_p = A.reshape(P, self.b, self.N)
+        self.d = int(self.M / self.P)
+        self.A_p = A.reshape(P, self.d, self.N)
         self.x = x
         Ax_p = self.set_Ax_p()
         SNRdB = 10**(-0.1*snr)
-        self.sigma = np.array([np.var(Ax_p[p]) for p in range(P)]) * SNRdB
-        self.n_p = np.array([np.random.normal(0, self.sigma[p]**0.5, (self.b, 1)) for p in range(P)])
+        self.sigma_p = np.array([np.var(Ax_p[p]) for p in range(P)]) * SNRdB
+        self.n_p = np.array([np.random.normal(0, self.sigma_p[p]**0.5, (self.d, 1)) for p in range(P)])
         self.y_p = Ax_p + self.n_p
         self.s = np.zeros((self.N, 1))
         self.mse = np.array([np.linalg.norm(self.s - self.x)**2 / self.N])
@@ -26,12 +26,12 @@ class D_AMP:
         return Ax
 
     def estimate(self, ite_max=20):
-        Onsager_p = np.zeros((self.P, self.b, 1))
+        Onsager_p = np.zeros((self.P, self.d, 1))
         for i in range(ite_max):
-            r = self.update_r()
-            w = self.update_w(r + Onsager_p)
+            r_p = self.update_r()
+            w_p = self.update_w(r + Onsager_p)
             v = self.update_v(r)
             t = self.update_t(v)
             self.s = self.update_s(w, t)
-            Onsager_p = np.sum(self.s != 0) / self.M * (r + Onsager_p)
+            Onsager_p = np.sum(self.s != 0) / self.M * (r_p + Onsager_p)
             self.mse = self.add_mse()
