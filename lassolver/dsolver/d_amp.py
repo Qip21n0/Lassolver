3113d8bfb07530b4596f7701788b1af1694e4cd0
Kenn <h.ken.40nuj7991@icloud.com>
2020-12-24 01:49:53 +0900

    fix class method

--- lassolver/dsolver/d_amp.py
+++ lassolver/dsolver/d_amp.py
@@ -1,6 +1,33 @@
 import numpy as np
 from lassolver.utils.utils import *
 
+class amp:
+    def __init__(self, A_p, x, snr):
+        self.A_p = A_p
+        self.d, self.N = A_p.shape
+        self.x = x
+        Ax_p = A_p @ x
+        SNRdB = 10**(-0.1*snr)
+        self.sigma_p = np.var(Ax_p) * SNRdB
+        n_p = np.random.normal(0, self.sigma_p**0.5, (self.d, 1))
+        self.y_p = Ax_p + n_p
+        self.s = np.zeros((self.N, 1))
+        self.AT_p = A_p.T
+        self.trA2_p = np.trace(self.AT_p @ self.A_p)
+        self.Onsager_p = np.zeros((self.d, 1))
+
+    def local_compute(self):
+        r = self.update_r()
+        w = self.update_w(r)
+        self.y_As = np.linalg.norm(r)**2
+
+    def update_r(self):
+        return self.y_p - self.A_p @ self.s
+
+    def update_w(self, r):
+        return self.AT_p @ (r + self.Onsager_p)
+        
+        
 class D_AMP:
     def __init__(self, A, x, snr, P):
         self.M, self.N = A.shape
