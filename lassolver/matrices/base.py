2aa300fa0a4026ac3c9ccaa89158742449dfa163
Kenn <h.ken.40nuj7991@icloud.com>
2020-12-25 23:44:18 +0900

    add function for matrix

--- lassolver/matrices/base.py
+++ lassolver/matrices/base.py
@@ -1,8 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 class Base:
     def __init__(self, M, N):
         self.M = M
         self.N = N
+        self.A = np.hstack((np.eye(M), np.zeros((M, N-M))))
 
-    def members(self):
-        for key in vars(self):
-            print("{}: {}".format(key, vars(self)[key]))
+    def matrix(self):
+        print("row: {}".format(self.M))
+        print("column: {}".format(self.N))
+        print("mean: {}".format(self.A.mean()))
+        print("variance: {}".format(self.A.var()))
+
+    def show_hist(self):
+        A = self.A.reshape(self.M * self.N)
+        plt.hist(A, bins=50)
