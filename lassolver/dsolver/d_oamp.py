743a0f027c4f06674d3218016b33c923d687db5f
Kenn <h.ken.40nuj7991@icloud.com>
2021-01-07 00:22:51 +0900

    complete D_OAMP

--- lassolver/dsolver/d_oamp.py
+++ lassolver/dsolver/d_oamp.py
@@ -1,6 +1,7 @@
 import numpy as np
 from lassolver.utils.func import *
 from lassolver.dsolver.d_base import *
+from lassolver.utils.utils import *
 
 class oamp(base):
     def __init__(self, A, x, snr, M):
@@ -9,22 +10,27 @@ class oamp(base):
     def receive_s(self, s):
         self.s = s
 
-    def local_compute(self, W):
+    def receive_W(self, W):
+        self.W = W.T
+
+    def local_compute(self):
         self.r = self._update_r()
-        w = self._update_w(W)
+        w = self._update_w()
         y_As = np.linalg.norm(self.r)**2
         return w, y_As
 
     def _update_r(self):
         return self.y - self.A @ self.s
 
-    def _update_w(self, W):
-        return W @ self.r
+    def _update_w(self):
+        return self.W @ self.r
 
 
 class D_OAMP(D_Base):
     def __init__(self, A, x, snr, P):
         super().__init__(A, x, snr, P)
+        self.A = A
+        self.AT = A.T
         self.AAT = A @ A.T
         self.I = np.eye(self.M)
         self.c = (self.N - self.M) / self.M
@@ -46,24 +52,56 @@ class D_OAMP(D_Base):
 
     def estimate(self, C=1.75, ord='LMMSE', ite_max=20):
         self.W = self.__set_W(1, ord)
+        self.W_p = self.W.T.reshape(self.P, self.Mp, self.N)
+        for p in range(self.P):
+            self.oamps[p].receive_W(self.W_p[p])
         w_p = np.zeros((self.P, self.N, 1))
         for i in range(ite_max):
             for p in range(self.P):
                 w_p[p], self.y_As_p[p] = self.oamps[p].local_compute()
             w_p[0] += self.s
             v = self._update_v()
-            t = self._update_t(v)
-            self.s = self._update_s(w_p, t)
+            t = self._update_t(v, ord)
+            self.s = self._update_s(C, w_p, t) if i != ite_max-1 else self._output_s(w_p, t)
             for p in range(self.P):
-                self.oamps[p].update_Onsager(self.s)
+                self.oamps[p].receive_s(self.s)
             self.mse = self._add_mse()
+            if ord == 'LMMSE':
+                self.W = self.__set_W(v, ord='LMMSE')
+                self.W_p = self.W.T.reshape(self.P, self.Mp, self.N)
+                for p in range(self.P):
+                    self.oamps[p].receive_W(self.W_p[p])
+
+    def __set_W(self, v, ord):
+        if ord == 'MF':
+            W_ = self.AT
+        elif ord == 'PINV':
+            W_ = np.linalg.pinv(self.A)
+        elif ord == 'LMMSE':
+            W_ = v * self.AT @ np.linalg.inv(v * self.AAT + self.sigma * self.I)
+        else :
+            raise NameError("not correct")
+        return self.N / np.trace(W_ @ self.A) * W_
 
     def _update_v(self):
         y_As = np.sum(self.y_As_p)
         return (y_As - self.M * self.sigma) / self.trA2
 
-    def _update_t(self, v):
-        return v / self.a + self.sigma
+    def _update_t(self, v, ord):
+        if ord == 'MF':
+            return v / self.a + self.sigma
+        elif ord == 'PINV':
+            if self.M < self.N:
+                return self.c * v + self.N / (self.N - self.M) * self.sigma
+            else :
+                return -self.c * self.sigma
+        else :
+            tmp = self.c * v + self.sigma
+            return (tmp + (tmp**2 + 4 * self.sigma * v)**0.5) / 2
 
-    def _update_s(self, w, t):
+    def _update_s(self, C, w, t):
+        w_ = np.sum(w, axis=0)
+        return C * DF(w_, t**0.5)
+
+    def _output_s(self, w, t):
         return GCAMP(w, t**0.5)
