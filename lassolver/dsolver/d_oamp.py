d18a7b04c2dcc480b532df627821ccfe158f1ffd
Kenn <h.ken.40nuj7991@icloud.com>
2021-01-09 02:02:26 +0900

    fix

--- lassolver/dsolver/d_oamp.py
+++ lassolver/dsolver/d_oamp.py
@@ -33,7 +33,6 @@ class D_OAMP(D_Base):
         self.AT = A.T
         self.AAT = A @ A.T
         self.I = np.eye(self.M)
-        self.c = (self.N - self.M) / self.M
         self.oamps = [oamp(self.A_p[p], x, snr, self.M) for p in range(P)]
         self.sigma = self.set_sigma()
         self.trA2 = self.set_trA2()
@@ -53,6 +52,9 @@ class D_OAMP(D_Base):
     def estimate(self, C=1.75, ord='LMMSE', ite_max=20):
         self.W = self.__set_W(1, ord)
         self.W_p = self.W.T.reshape(self.P, self.Mp, self.N)
+        B = np.eye(self.N) - self.W @ self.A
+        self.trW2 = np.trace(self.W @ self.W.T)
+        self.trB2 = np.trace(B @ B.T)
         for p in range(self.P):
             self.oamps[p].receive_W(self.W_p[p])
         w_p = np.zeros((self.P, self.N, 1))
@@ -61,7 +63,7 @@ class D_OAMP(D_Base):
                 w_p[p], self.y_As_p[p] = self.oamps[p].local_compute()
             w_p[0] += self.s
             v = self._update_v()
-            t = self._update_t(v, ord)
+            t = self._update_t(v)
             self.s = self._update_s(C, w_p, t) if i != ite_max-1 else self._output_s(w_p, t)
             for p in range(self.P):
                 self.oamps[p].receive_s(self.s)
@@ -69,6 +71,9 @@ class D_OAMP(D_Base):
             if ord == 'LMMSE':
                 self.W = self.__set_W(v, ord='LMMSE')
                 self.W_p = self.W.T.reshape(self.P, self.Mp, self.N)
+                B = np.eye(self.N) - self.W @ self.A
+                self.trW2 = np.trace(self.W @ self.W.T)
+                self.trB2 = np.trace(B @ B.T)
                 for p in range(self.P):
                     self.oamps[p].receive_W(self.W_p[p])
 
@@ -87,17 +92,8 @@ class D_OAMP(D_Base):
         y_As = np.sum(self.y_As_p)
         return (y_As - self.M * self.sigma) / self.trA2
 
-    def _update_t(self, v, ord):
-        if ord == 'MF':
-            return v / self.a + self.sigma
-        elif ord == 'PINV':
-            if self.M < self.N:
-                return self.c * v + self.N / (self.N - self.M) * self.sigma
-            else :
-                return -self.c * self.sigma
-        else :
-            tmp = self.c * v + self.sigma
-            return (tmp + (tmp**2 + 4 * self.sigma * v)**0.5) / 2
+    def _update_t(self, v):
+        return 1/self.N * (self.trB2 * v + self.trW2 * self.sigma)
 
     def _update_s(self, C, w, t):
         w_ = np.sum(w, axis=0)
--- lassolver/solver/oamp.py
+++ lassolver/solver/oamp.py
@@ -7,21 +7,24 @@ class OAMP(AMP):
         super().__init__(A, x, snr)
         self.AAT = A @ A.T
         self.I = np.eye(self.M)
-        self.c = (self.N - self.M) / self.M
 
     def estimate(self, C=1.75, ord='LMMSE', ite_max=20):
         self.W = self.__set_W(1, ord)
-        self.B = np.eye(self.N) - self.W @ self.A
+        B = np.eye(self.N) - self.W @ self.A
+        self.trW2 = np.trace(self.W @ self.W.T)
+        self.trB2 = np.trace(B @ B.T)
         for i in range(ite_max):
             r = self._update_r()
             w = self._update_w(r)
             v = self._update_v(r)
-            t = self._update_t(v, ord)
+            t = self._update_t(v)
             self.s = self._update_s(C, w, t) if i != ite_max-1 else self._output_s(w, t)
             self.mse = self._add_mse()
             if ord == 'LMMSE':
                 self.W = self.__set_W(v, ord='LMMSE')
-                self.B = np.eye(self.N) - self.W @ self.A
+                B = np.eye(self.N) - self.W @ self.A
+                self.trW2 = np.trace(self.W @ self.W.T)
+                self.trB2 = np.trace(B @ B.T)
 
     def __set_W(self, v, ord):
         if ord == 'MF':
@@ -37,20 +40,8 @@ class OAMP(AMP):
     def _update_w(self, r):
         return self.s + self.W @ r
 
-    def _update_t(self, v, ord):
-        return 1/self.N * (np.trace(self.B @ self.B.T)*v + np.trace(self.W @ self.W.T)*self.sigma)
-        """
-        if ord == 'MF':
-            return v / self.a + self.sigma
-        elif ord == 'PINV':
-            if self.M < self.N:
-                return self.c * v + self.N / (self.N - self.M) * self.sigma
-            else :
-                return -self.c * self.sigma
-        else :
-            tmp = self.c * v + self.sigma
-            return (tmp + (tmp**2 + 4 * self.sigma * v)**0.5) / 2
-        """
+    def _update_t(self, v):
+        return 1/self.N * (self.trB2 * v + self.trW2 * self.sigma)
 
     def _update_s(self, C, w, t):
         return C * DF(w, t**0.5)
