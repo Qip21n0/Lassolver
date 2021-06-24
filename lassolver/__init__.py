0312b353f5d2b695764d2f3721120a543db21331
Kenn <h.ken.40nuj7991@icloud.com>
2021-06-24 21:54:45 +0900

    make setup file

--- lassolver/__init__.py
+++ lassolver/__init__.py
@@ -2,10 +2,3 @@ from lassolver.matrices import *
 from lassolver.solver import *
 from lassolver.dsolver import *
 from lassolver.utils import *
-
-def version():
-    version = '0.0.0'
-    
-    return """
-    Lassolver ver {}.
-    """.format(version)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'lassolver',
+	version = '0.1.0',
+	author = 'Ken Hisanaga',
+	description='Lassolver is a Python package for Compressed Sensing and Distributed Compressed Sensing',
+	install_requires = ['numpy','scipy','matplotlib',],
+	packages = ['lassolver'],
+	url = 'https://github.com/Qip21n0/Lassolver',
+	license = 'MIT',
+)
