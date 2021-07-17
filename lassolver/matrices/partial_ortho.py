aef95371af82f8d1fdecfcc411ae9bfd808cbe47
Kenn <h.ken.40nuj7991@icloud.com>
2021-07-18 00:24:17 +0900

    modified

--- lassolver/matrices/partial_ortho.py
+++ lassolver/matrices/partial_ortho.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 from scipy.stats import ortho_group
 from scipy.fftpack import dct
 from scipy.linalg import hadamard
@@ -8,8 +9,8 @@ class PartialOrtho(Base):
     def __init__(self, M, N, ord='Haar'):
         super().__init__(M, N)
         self.I = np.eye(N)
-        s = np.random.randint(0, N, M)
-        self.S = np.array([self.I[i] for i in s])
+        rows = random.sample([i for i in range(N)], M)
+        self.S = np.array([self.I[row] for row in rows])
         self.U = self.__set_U(ord)
         self.A = (N/M)**0.5 * self.S @ self.U.T
 
