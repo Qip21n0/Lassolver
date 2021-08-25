0214c16175117f831690a6ecb4aa9f3ca6cb6160
Kenn <h.ken.40nuj7991@icloud.com>
2021-08-26 03:22:57 +0900

    updated estimate func

--- lassolver/dsolver/d_amp.py
+++ lassolver/dsolver/d_amp.py
@@ -7,6 +7,9 @@ class damp(dbase):
         super().__init__(A_p, x, snr, M)
         self.Onsager_p = np.zeros((self.M_p, 1))
 
+    def receive_trA2(self, trA2):
+        self.trA2 = trA2
+
     def update_Onsager(self, s):
         self.s = s
         self.Onsager_p = np.sum(self.s != 0) / self.M * (self.r_p + self.Onsager_p)
@@ -15,13 +18,16 @@ class damp(dbase):
         self.r_p = self._update_r_p()
         w_p = self._update_w_p()
         r2_p = np.linalg.norm(self.r_p)**2
-        return w_p, r2_p
+        v_p = (np.linalg.norm(self.r_p)**2 - self.M * self.sigma_p) / self.trA2
+        if v_p < 0: v_p = 1e-5
+        tau_p = self.N / self.M * v_p + self.sigma_p
+        return w_p, v_p, tau_p
 
     def _update_r_p(self):
         return self.y - self.A_p @ self.s
 
     def _update_w_p(self):
-        return self.AT_p @ (self.r_p + self.Onsager_p)
+        return self.s / self.P + self.AT_p @ (self.r_p + self.Onsager_p)
         
         
 class D_AMP(D_Base):
@@ -44,30 +50,37 @@ class D_AMP(D_Base):
         return trA2
 
     def estimate(self, T=20, log=False):
-        lambda_ = np.linspace(3, 2, T)
         w = np.zeros((self.P, self.N, 1))
         self.communication_cost = np.empty(0)
 
+        for p in range(self.P):
+            self.amps[p].receive_trA2(self.trA2)
+
         for t in range(T):
             for p in range(self.P):
-                w[p], self.r2[p] = self.amps[p].local_compute()
-            w[0] += self.s
+                w[p], self.v[p], self.tau[p] = self.amps[p].local_compute()
+            #w[0] += self.s
             v = self._update_v()
-            tau = self._update_tau(v)
-            self._update_s(w, lambda_[t], tau, log)
+            tau = self._update_tau()
+            self.www.append([w.copy(), self.tau.copy()])
+            if log: print("{}/{}: tau = {}, v = {}".format(t+1, T, tau, v))
+            self._update_s(w, log)
 
             for p in range(self.P):
                 self.amps[p].update_Onsager(self.s)
             self._add_mse()
 
     def _update_v(self):
-        r2 = np.sum(self.r2)
-        return (r2 - self.M * self.sigma) / self.trA2
+        #r2 = np.sum(self.r2)
+        #v = (r2 - self.M * self.sigma) / self.trA2
+        #return v if v > 0 else 1e-4
+        return np.sum(self.v)
 
-    def _update_tau(self, v):
-        return v / self.a + self.sigma
+    def _update_tau(self):
+        #return v / self.a + self.sigma
+        return np.sum(self.tau)
 
-    def _update_s(self, w, lambda_, tau, log):
-        s, communication_cost = GCAMP(w, lambda_, tau**0.5, log)
+    def _update_s(self, w, log):
+        s, communication_cost = GCAMP(w, self.tau, log)
         self.s = s
         self.communication_cost = np.append(self.communication_cost, communication_cost)
--- lassolver/dsolver/d_oamp.py
+++ lassolver/dsolver/d_oamp.py
@@ -12,21 +12,31 @@ class doamp(dbase):
     def receive_W_p(self, W_p):
         self.W_p = W_p.copy().T
 
+    def receive_trX2(self, trW2, trB2):
+        self.trW2 = trW2
+        self.trB2 = trB2
+
+    def receive_trA2(self, trA2):
+        self.trA2 = trA2
+
     def local_compute(self):
         self.r_p = self._update_r_p()
         w_p = self._update_w_p()
         r2_p = np.linalg.norm(self.r_p)**2
-        return w_p, r2_p
+        v_p = (r2_p - self.M * self.sigma_p) / self.trA2
+        if v_p < 0: v_p = 1e-5
+        tau_p = 1/self.N * (self.trB2 * v_p + self.trW2 * self.sigma_p)
+        return w_p, v_p, tau_p, r2_p
 
     def _update_r_p(self):
         return self.y - self.A_p @ self.s
 
     def _update_w_p(self):
-        return self.W_p @ self.r_p
+        return self.s / self.P + self.W_p @ self.r_p
 
 
 class D_OAMP(D_Base):
-    def __init__(self, A, x, snr, P, iidG=False):
+    def __init__(self, A, x, snr, P):
         super().__init__(A, x, snr, P)
         self.A = A.copy()
         self.AT = self.A.T
@@ -36,7 +46,6 @@ class D_OAMP(D_Base):
         self.oamps = [doamp(self.A_p[p], x, snr, self.M) for p in range(self.P)]
         self.sigma = self.__set_sigma()
         self.trA2 = self.__set_trA2()
-        self.iidG = iidG
 
     def __set_sigma(self):
         sigma = 0
@@ -51,29 +60,32 @@ class D_OAMP(D_Base):
         return trA2
 
     def estimate(self, T=20, C=2.0, ord='LMMSE', log=False, approx=False):
-        lambda_ = np.linspace(3, 1, T)
-        lambda_[:] = 1
         w = np.zeros((self.P, self.N, 1))
         self.communication_cost = np.empty(0)
 
-        self.W = self.__set_W(1, ord)
+        v = (sum([np.linalg.norm(self.oamps[p].y)**2 for p in range(self.P)]) - self.M * self.sigma) / self.trA2
+        self.W = self.__set_W(v, ord)
         self.W_p = self.W.T.reshape(self.P, self.M_p, self.N)
-        if not self.iidG:
-            I = np.eye(self.N)
-            B = I - self.W @ self.A
-            self.trW2 = np.trace(self.W @ self.W.T)
-            self.trB2 = np.trace(B @ B.T)
+        
+        I = np.eye(self.N)
+        B = I - self.W @ self.A
+        self.trW2 = np.trace(self.W @ self.W.T)
+        self.trB2 = np.trace(B @ B.T)
         for p in range(self.P):
             self.oamps[p].receive_W_p(self.W_p[p])
+            self.oamps[p].receive_trX2(self.trW2, self.trB2)
+            self.oamps[p].receive_trA2(self.trA2)
         
         for t in range(T):
             for p in range(self.P):
-                w[p], self.r2[p] = self.oamps[p].local_compute()
-            w[0] += self.s
+                w[p], self.v[p], self.tau[p], self.r2[p] = self.oamps[p].local_compute()
+            #w[0] += self.s
             v = self._update_v()
-            tau = self._update_tau(v, ord)
+            tau = self._update_tau(v)
+            self.www.append([w.copy(), self.tau.copy()])
+            if log: print("{}/{}: tau = {}, v = {}".format(t+1, T, tau, v))
             if t == T-1: break
-            self._update_s(C, w, lambda_[t], tau, log, approx)
+            self._update_s(C, w, log)
 
             for p in range(self.P):
                 self.oamps[p].receive_s(self.s)
@@ -82,14 +94,14 @@ class D_OAMP(D_Base):
             if ord == 'LMMSE':
                 self.W = self.__set_W(v, ord='LMMSE')
                 self.W_p = self.W.T.reshape(self.P, self.M_p, self.N)
-                if not self.iidG:
-                    B = I - self.W @ self.A
-                    self.trW2 = np.trace(self.W @ self.W.T)
-                    self.trB2 = np.trace(B @ B.T)
+                B = I - self.W @ self.A
+                self.trW2 = np.trace(self.W @ self.W.T)
+                self.trB2 = np.trace(B @ B.T)
                 for p in range(self.P):
                     self.oamps[p].receive_W_p(self.W_p[p])
+                    self.oamps[p].receive_trX2(self.trW2, self.trB2)
         
-        self._output_s(w, lambda_[-1], tau, log)
+        self._output_s(w, log)
         self._add_mse()
 
     def __set_W(self, v, ord):
@@ -105,29 +117,18 @@ class D_OAMP(D_Base):
 
     def _update_v(self):
         r2 = np.sum(self.r2)
-        return (r2 - self.M * self.sigma) / self.trA2
+        v = (r2 - self.M * self.sigma) / self.trA2
+        return v if v > 0 else 1e-4
 
-    def _update_tau(self, v, ord):
-        if self.iidG:
-            if ord == 'MF':
-                return v / self.a + self.sigma
-            elif ord == 'PINV':
-                if self.M < self.N:
-                    return self.c * v + self.N / (self.N - self.M) * self.sigma
-                else :
-                    return -self.c * self.sigma
-            else :
-                tmp = self.c * v + self.sigma
-                return (tmp + (tmp**2 + 4 * self.sigma * v)**0.5) / 2
-        else :
-            return 1/self.N * (self.trB2 * v + self.trW2 * self.sigma)
+    def _update_tau(self, v):
+        return 1/self.N * (self.trB2 * v + self.trW2 * self.sigma)
 
-    def _update_s(self, C, w, lambda_, tau, log, approx):
-        s, communication_cost = GCOAMP(w, lambda_, tau**0.5, log, approx)
+    def _update_s(self, C, w, log):
+        s, communication_cost = GCOAMP(w, self.tau, log)
         self.s = C * s
         self.communication_cost = np.append(self.communication_cost, communication_cost)
 
-    def _output_s(self, w, lambda_, tau, log):
-        s, communication_cost = GCAMP(w, lambda_, tau**0.5, log)
+    def _output_s(self, w, log):
+        s, communication_cost = GCAMP(w, self.tau, log)
         self.s = s
         self.communication_cost = np.append(self.communication_cost, communication_cost)
