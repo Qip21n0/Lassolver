6dd87e64109e5e520bbdf69351b03c358c0c0860
Kenn <h.ken.40nuj7991@icloud.com>
2022-04-14 02:57:19 +0900

    modified Node class

--- lassolver/solver/ista.py
+++ lassolver/solver/ista.py
@@ -10,6 +10,11 @@ class ISTA(Node):
         super().__init__(A, x, snr)
         self.r = jnp.zeros(self.M)
         self.w = jnp.zeros(self.N)
+        self.s = jnp.zeros(self.N, dtype=jnp.float32)
+        self.s_history = []
+
+        self.AT = self.A.T
+        self.mse = [None]
 
 
     def estimate(self, T=20, tau=0.5):
@@ -39,3 +44,27 @@ class ISTA(Node):
 
     def _update_s(self, gamma):
         self.s = soft_threshold(self.w, gamma)
+
+
+    def _add_mse(self):
+        mse = np.linalg.norm(self.s - self.x)**2 / self.N
+        self.mse.append(mse)
+
+
+    def show_result(self):
+        print("final mse: {}".format(self.mse[-1]))
+        plt.figure(figsize=(16, 4))
+
+        plt.subplot(121)
+        plt.plot(self.x.real)
+        plt.plot(self.s.real)
+        plt.grid()
+        
+        plt.subplot(122)
+        plt.xlabel('iteration')
+        plt.ylabel('MSE[log10]')
+        iter = np.shape(self.mse)[0]
+        plt.xticks(np.arange(0, iter, 1))
+        result = np.array([np.log10(mse) if mse is not None else None for mse in self.mse])
+        plt.plot(result)
+        plt.grid()
--- lassolver/utils/node.py
+++ lassolver/utils/node.py
@@ -16,32 +16,3 @@ class Node:
 		self.n = np.random.normal(0, self.sigma**0.5, (self.M, 1))
 
 		self.y = Ax + self.n
-		self.s = jnp.zeros(self.N, dtype=jnp.float32)
-		self.s_history = []
-
-		self.AT = self.A.T
-		self.mse = [None]
-
-
-	def _add_mse(self):
-		mse = np.linalg.norm(self.s - self.x)**2 / self.N
-		self.mse.append(mse)
-
-
-	def show_result(self):
-		print("final mse: {}".format(self.mse[-1]))
-		plt.figure(figsize=(16, 4))
-
-		plt.subplot(121)
-		plt.plot(self.x.real)
-		plt.plot(self.s.real)
-		plt.grid()
-		
-		plt.subplot(122)
-		plt.xlabel('iteration')
-		plt.ylabel('MSE[log10]')
-		iter = np.shape(self.mse)[0]
-		plt.xticks(np.arange(0, iter, 1))
-		result = np.array([np.log10(mse) if mse is not None else None for mse in self.mse])
-		plt.plot(result)
-		plt.grid()
