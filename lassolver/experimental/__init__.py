4aa76d611cba0be12ffa804ca6cce4b474e5c8af
Kenn <h.ken.40nuj7991@icloud.com>
2022-07-12 02:27:07 +0900

    add DOAMP for verifivation

--- lassolver/experimental/__init__.py
+++ lassolver/experimental/__init__.py
@@ -1,3 +1,4 @@
 from lassolver.experimental.d_amp_exp import *
+from lassolver.experimental.d_oamp_exp import *
 from lassolver.experimental.d_amp_nc3 import *
 from lassolver.experimental.d_oamp_nc3 import *
