59efe7e0b86844e867d607ddd13c4c6cddf645d5
Kenn <h.ken.40nuj7991@icloud.com>
2022-08-01 14:02:35 +0900

    modified plt func for confusion matrix & removed plt func for impact table

--- lassolver/utils/utils.py
+++ lassolver/utils/utils.py
@@ -56,76 +56,7 @@ def plt_SE(se, T, color=None):
     plt.scatter(step, se, c=color)
 
 
-def plt_MSE_and_hist(target, T, limit=False):
-    n = T//11
-    flag = False
-    if n * 11 < T:
-        n += 1
-        flag = True
-
-    plt.figure(figsize=(14, 6*n))
-    for i in range(n+1):
-        t = 11 * i - 1 if i != 0 else 0
-        if i == n and flag:
-            t = -1
-        mse_all, hist_all, bins_all = target.mse_hist_bins[t][0].copy()
-        mse_zero, hist_zero, bins_zero = target.mse_hist_bins[t][1].copy()
-        mse_non_zero, hist_non_zero, bins_non_zero = target.mse_hist_bins[t][2].copy()
-
-        plt.subplot(n+1, 2, 2*i+1)
-        plt.title(f'MSE (t = {str(t+1)})')
-        plt.plot(bins_all, mse_all, label="all")
-        plt.plot(bins_zero, mse_zero, label="x = 0")
-        plt.plot(bins_non_zero, mse_non_zero, label="x != 0")
-        if limit:
-            upper = np.max(mse_all)
-            plt.ylim(None, upper * 1.1)
-        plt.yscale('log')
-        plt.legend()
-        plt.grid()
-
-        plt.subplot(n+1, 2, 2*(i+1))
-        plt.title(f'Quantity (t = {str(t+1)})')
-        plt.plot(bins_all, hist_all, label="all")
-        plt.plot(bins_zero, hist_zero, label="x = 0")
-        plt.plot(bins_non_zero, hist_non_zero, label="x != 0")
-        plt.legend()
-        plt.grid()
-
-
-def plt_s_diff_non_zero(target, T):
-    n = T//11
-    flag = False
-    if n * 11 < T:
-        n += 1
-        flag = True
-
-    plt.figure(figsize=(14, 6*n))
-    for i in range(n+1):
-        t = 11 * i - 1 if i != 0 else 0
-        if i == n and flag:
-            t = -1
-        s = target.s_history_4_diff_non_zero[t].real.copy()
-        s_without_None = np.array([])
-        for s_comp in s:
-            if s_comp is not None:
-                s_without_None = np.append(s_without_None, s_comp)
-        hist, bins = np.histogram(s_without_None, bins=50)
-        hist = np.append(hist, 0)
-
-        plt.subplot(n+1, 2, 2*i+1)
-        plt.title(f's (t = {str(t+1)})')
-        plt.plot(target.x)
-        plt.plot(s)
-        plt.grid()
-
-        plt.subplot(n+1, 2, 2*(i+1))
-        plt.title(f's histogram (t = {str(t+1)})')
-        plt.plot(bins, hist)
-        plt.grid()
-
-
-def plt_heatmap(impact_table, T):
+def plt_heatmap(confusion_matrix, T):
     n = T//11
     flag = False
     if n * 11 < T:
@@ -140,33 +71,30 @@ def plt_heatmap(impact_table, T):
         if i == n and flag:
             t = -1
         
-        s = impact_table[t][1].astype('int')
+        s = confusion_matrix[t][1].astype('int')
         plt.subplot(row, 2, i+1)
         plt.title(f'Quantity (t = {str(t+1)})')
         sns.heatmap(s, cmap="RdBu_r", xticklabels=["x = 0", "x ≠ 0", "sum(diff)"], yticklabels=["diff = 0", "diff ≠ 0", "sum(x)"], annot=True, fmt='d', annot_kws={"fontsize": 12})
 
 
-def plt_MSE_impact_table(impact_table):
-    T = len(impact_table)
+def plt_MSE_confusion_matrix(confusion_matrix):
+    T = len(confusion_matrix)
     mse_of = {}
-    impact_table = np.array(impact_table)
+    cm = np.array(confusion_matrix)
 
-    mse_of["x=0 & diff=0"] = impact_table[:, 0, 0, 0]
-    mse_of["x!=0 & diff=0"] = impact_table[:, 0, 0, 1]
-    mse_of["x=0 & diff!=0"] = impact_table[:, 0, 1, 0]
-    mse_of["x=!0 & diff!=0"] = impact_table[:, 0, 1, 1]
+    mse_of["x=0 & diff=0"] = cm[:, 0, 0, 0] # TP
+    mse_of["x!=0 & diff=0"] = cm[:, 0, 0, 1] # FP
+    mse_of["x=0 & diff!=0"] = cm[:, 0, 1, 0] # FN
+    mse_of["x=!0 & diff!=0"] = cm[:, 0, 1, 1] # TN
 
-    mse_of["diff=0"] = impact_table[:, 0, 0, 2]
-    mse_of["diff!=0"] = impact_table[:, 0, 1, 2]
-    mse_of["x=0"] = impact_table[:, 0, 2, 0]
-    mse_of["x!=0"] = impact_table[:, 0, 2, 1]
+    mse_of["diff=0"] = cm[:, 0, 0, 2]
+    mse_of["diff!=0"] = cm[:, 0, 1, 2]
+    mse_of["x=0"] = cm[:, 0, 2, 0]
+    mse_of["x!=0"] = cm[:, 0, 2, 1]
 
-    mse_of["all"] = impact_table[:, 0, 2, 2]
+    mse_of["all"] = cm[:, 0, 2, 2]
 
     step = np.arange(0, T+1, 5)
-    dot = [i for i in range(T) if i % 5 == 0]
-    if dot[-1] != T-1:
-        dot.append(T-1)
 
     plt.xlabel("iteration")
     plt.ylabel("MSE")
@@ -176,21 +104,19 @@ def plt_MSE_impact_table(impact_table):
 
     for k, v in mse_of.items():
         linestyle = '-'
-        marker = None
-        mkv = None
+        color = 'tab:green'
+
         if 'x=0' in k:
             linestyle = '--'
         elif 'x!=' in k:
             linestyle = '-.'
 
         if 'diff=0' in k:
-            marker = 'o'
-            mkv = dot
+            color = 'tab:red'
         elif 'diff!=0' in k:
-            marker = '*'
-            mkv = dot
+            color = 'tab:blue'
         
-        plt.plot(v, label=k, linestyle=linestyle, marker=marker, markevery=mkv)
+        plt.plot(v, label=k, linestyle=linestyle, color=color)
     
     plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
     plt.grid()
