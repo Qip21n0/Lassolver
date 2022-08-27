016345ae9f1213c491dc11c8eb117e4d3e028af5
Kenn <h.ken.40nuj7991@icloud.com>
2022-08-27 21:46:16 +0900

    modified plt funcs for MSE of s_w and s_b

--- lassolver/utils/utils.py
+++ lassolver/utils/utils.py
@@ -317,7 +317,8 @@ def plt_w_b_scatter(target, type, T):
         cond1 = w[type] <= -tau[t]
         cond2 = np.logical_and(-tau[t] < w[type], w[type] <= tau[t])
         cond3 = tau[t] < w[type]
-        x = np.linspace(np.nanmin(w[type]), np.nanmax(w[type]), 1000)
+        cond = np.logical_and(cond1, cond2, cond3)
+        x = np.linspace(np.nanmin(s_w[cond]), np.nanmax(s_w[cond]), 1000)
 
         plt.subplot(n+1, 1, i+1)
         plt.title(type+f" (t = {str(t+1)})")
