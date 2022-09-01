b5d122c602c808b6edfff64ede1a743ece45514c
Kenn <h.ken.40nuj7991@icloud.com>
2022-09-01 21:34:38 +0900

    modified DAMP with SP

--- lassolver/dsolver/d_amp_sp.py
+++ lassolver/dsolver/d_amp_sp.py
@@ -113,7 +113,7 @@ class D_AMP_SP(D_Base):
         mse = np.zeros(self.P)
         for p in range(self.P):
             mse[p] = np.linalg.norm(self.amps[p].s - self.x)**2 / self.N
-        self.mse = np.append(self.mse, mse)
+        self.mse = np.append(self.mse, mse, axis=0)
 
     def result(self):
         print("final mse: {}".format(self.mse[-1]))
