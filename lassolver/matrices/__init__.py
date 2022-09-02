693ba234cbabf6b2628f99dfe533f0f8eb0b02ad
Kenn <h.ken.40nuj7991@icloud.com>
2022-09-02 13:29:46 +0900

    add adjaency matrix

--- lassolver/matrices/__init__.py
+++ lassolver/matrices/__init__.py
@@ -2,3 +2,4 @@ from lassolver.matrices.base import *
 from lassolver.matrices.iid_gauss import *
 from lassolver.matrices.uni_invar import *
 from lassolver.matrices.partial_ortho import *
+from lassolver.matrices.adjacency import *
