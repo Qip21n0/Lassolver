c0d3aa51d145f696a295373ad2453913bdfbd9a2
Kenn <h.ken.40nuj7991@icloud.com>
2022-11-04 15:28:31 +0900

    updated to ver.0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'lassolver',
-	version = '0.3.0',
+	version = '0.3.1',
 	author = 'Ken Hisanaga',
 	description='Lassolver is a Python package for Compressed Sensing and Distributed Compressed Sensing',
 	install_requires = ['numpy','scipy','matplotlib','networkx',],
