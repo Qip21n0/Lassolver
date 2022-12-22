ba410a6f21cea6c0dc540fd5f3e23b2b1d884944
Kenn <h.ken.40nuj7991@icloud.com>
2022-12-22 22:57:16 +0900

    removed log for debug in communication process

--- lassolver/dsolver/d_amp_sp.py
+++ lassolver/dsolver/d_amp_sp.py
@@ -87,14 +87,12 @@ class D_AMP_SP(D_Base):
                 if rand:
                     np.random.shuffle(order)
                 for p in order:
-                    #w_pp[p], v_pp[p], tau_pp[p] = self.summation_propagation(p, w_pp[:, p], v_pp[:, p], tau_pp[:, p])
-                    s1, s2, s3 = self.summation_propagation(p, w_pp[:, p], v_pp[:, p], tau_pp[:, p])
-                    for j, islinekd in enumerate(self.Adj[p]):
-                        if islinekd == 1:
-                            w_pp[p][j] = np.sum(w_pp[:, p], axis=0) - w_pp[j][p]
-                            v_pp[p][j] = np.sum(v_pp[:, p]) - v_pp[j][p]
-                            tau_pp[p][j] = np.sum(tau_pp[:, p]) - tau_pp[j][p]
-                    print(np.linalg.norm(s1 - w_pp[p]), np.linalg.norm(s2 - v_pp[p]), np.linalg.norm(s3 - tau_pp[p]))
+                    w_pp[p], v_pp[p], tau_pp[p] = self.summation_propagation(p, w_pp[:, p], v_pp[:, p], tau_pp[:, p])
+                    #for j, islinekd in enumerate(self.Adj[p]):
+                    #    if islinekd == 1:
+                    #        w_pp[p][j] = np.sum(w_pp[:, p], axis=0) - w_pp[j][p]
+                    #        v_pp[p][j] = np.sum(v_pp[:, p]) - v_pp[j][p]
+                    #        tau_pp[p][j] = np.sum(tau_pp[:, p]) - tau_pp[j][p]
             v = self._update_v(v_pp)
             tau = self._update_tau(tau_pp)
             if log: 
