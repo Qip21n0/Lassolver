8f70799ea549a9d1d5c1495ccd72048476362d50
Kenn <h.ken.40nuj7991@icloud.com>
2023-01-29 04:17:34 +0900

    modified an argument in dfunc_mmse

--- lassolver/solver/amp_opt.py
+++ lassolver/solver/amp_opt.py
@@ -31,7 +31,7 @@ class AMP_OPT(ISTA):
             self.s = self._update_s(w, tau)
 
             rho = np.mean(soft_threshold(w, tau**0.5) != 0)
-            Onsager = np.sum(dfunc_mmse(w.reshape((self.N,)), tau**0.5, rho)) / self.M * (r + Onsager)
+            Onsager = np.sum(dfunc_mmse(w.reshape((self.N,)), tau, rho)) / self.M * (r + Onsager)
             print(np.linalg.norm(self.s), np.linalg.norm(Onsager))
             self._add_mse()
 
