75ef358416a7217387c692168133ba4631057847
Kenn <h.ken.40nuj7991@icloud.com>
2023-01-28 20:41:43 +0900

    updated to ver.0.3.2-ssp

--- lassolver/solver/__init__.py
+++ lassolver/solver/__init__.py
@@ -1,4 +1,6 @@
 from lassolver.solver.ista import *
 from lassolver.solver.fista import *
 from lassolver.solver.amp import *
+from lassolver.solver.amp_opt import *
 from lassolver.solver.oamp import *
+from lassolver.solver.oamp_opt import *
